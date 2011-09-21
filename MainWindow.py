bee30b60dd702f751b2894f7f54b34fcee808fd4
Young-Ho Cha <ganadist@gmail.com>
2011-09-22 00:54:50 +0900

    finally it works.
    	- implements various user interface
    	- clean up codes
    
    Signed-off-by: Young-Ho Cha <ganadist@gmail.com>

--- MainWindow.py
+++ MainWindow.py
@@ -2,7 +2,7 @@ from gi.repository import GLib, GObject, Gtk, Vte
 import utils
 import os, sys
 
-SOURCE_TOP = "/home/ganadist/android/src"
+COL_ENABLE, COL_RULE, COL_DESCRITION = range(3)
 
 class MainWindow:
 	def __init__(self):
@@ -11,29 +11,60 @@ class MainWindow:
 		self.win = self.ui.get_object("window1")
 		self.win.connect("destroy", Gtk.main_quit)
 
-		expander = self.ui.get_object("TerminalExpander")
+		scroll = self.ui.get_object("TerminalScroll")
 		self.terminal = Vte.Terminal()
 		self.terminal.set_size(80, 25)
 		self.terminal.connect("child-exited", self.build_terminated)
-		expander.add(self.terminal)
+		scroll.add_with_viewport(self.terminal)
 
 		self.build_btn = self.ui.get_object("btn_build")
 		self.build_btn.connect("toggled", self.toggle_btn_build)
 
-		self.setSourceTop(SOURCE_TOP)
-		self.pid = None
+		treeview = self.ui.get_object("treeview_rules")
+		treeview.connect("row-activated", self.activate_rule_row)
+		renderer = self.ui.get_object("renderer_enable")
+		renderer.connect("toggled", self.toggle_enable_rule)
 
+		chooser = self.ui.get_object("chooser_source_top")
+		chooser.connect("file-set", self.choose_source_top)
+		chooser.connect("file-set", self.choose_source_top)
+
+		self.pid = None
 		self.win.show_all()
 
-	def setSourceTop(self, directory):
-		products = utils.getProductsList(directory)
+	def choose_source_top(self, chooser):
+		directory = chooser.get_filename()
+		self.set_source_top(directory)
+
+	def set_choose_directory(self, directory):
+		chooser = self.ui.get_object("chooser_source_top")
+		chooser.set_filename(directory)
+
+	def set_ui_enable(self, enable):
+		for name in ("btn_build", "combobox_product", "combobox_variant"):
+			w = self.ui.get_object(name)
+			w.set_sensitive(enable)
+
+	def set_source_top(self, directory):
+		print 'set ', directory
+		if not utils.check_source_top(directory):
+			self.set_ui_enable(False)
+			return False
+
+		products = utils.get_product_list(directory)
+		if not products:
+			self.set_ui_enable(False)
+			return False
 		combo = self.ui.get_object("combobox_product")
-		if products:
-			for product in products:
-				combo.append_text(product)
-			combo.set_active(0)
+		for product in products:
+			combo.append_text(product)
+		combo.set_active(0)
 		combo = self.ui.get_object("combobox_variant")
 		combo.set_active(0)
+
+		self.sourceTop = directory
+		self.set_ui_enable(True)
+		return True
 
 	def toggle_btn_build(self, button):
 		if button.get_active():
@@ -41,6 +72,18 @@ class MainWindow:
 		else:
 			self.stop_build()
 		return True
+
+	def toggle_column_enable(self, path):
+		model = self.ui.get_object("liststore_rules")
+		iter = model.get_iter(path)
+		active = model.get(iter, COL_ENABLE)[0]
+		model.set(iter, COL_ENABLE, not active)
+
+	def activate_rule_row(self, treeview, path, column):
+		self.toggle_column_enable(path)
+
+	def toggle_enable_rule(self, cell, path):
+		self.toggle_column_enable(path)
 
 	def stop_build(self):
 		self.build_btn.set_label("Build")
@@ -51,17 +94,39 @@ class MainWindow:
 	def run_build(self):
 		self.build_btn.set_label("Stop")
 		self.terminal.reset(True, True)
+
 		product = self.ui.get_object("combobox_product").get_active_text()
 		variant = self.ui.get_object("combobox_variant").get_active_text()
-		lunch = "lunch %s-%s"%(product, variant)
-		script = lunch + ";make bootimage"
+		script = "lunch %s-%s"%(product, variant)
+		script += ";" + "export OUT_DIR=" + "-".join(("out", product, variant))
+		if self.ui.get_object("btn_ccache").get_active():
+			script += ";" + "export USE_CCACHE=1"
+		make_opts = []
+
+		if self.ui.get_object("btn_parallel_build").get_active():
+			cpus = os.sysconf("SC_NPROCESSORS_ONLN")
+			make_opts.append("-j%d"%(cpus + 1))
+		if self.ui.get_object("btn_clean").get_active():
+			make_opts.append("clean")
+		if self.ui.get_object("btn_verbose").get_active():
+			make_opts.append("showcommands")
+
+		model = self.ui.get_object("liststore_rules")
+		def model_iter(model, path, iter, user_data):
+			active, rule = model.get(iter, COL_ENABLE, COL_RULE)
+			if active:
+				make_opts.append(rule)
+
+		model.foreach(model_iter, None)
+
+		script += ";" + "make bootimage " + " ".join(make_opts)
 		print 'run:', script
 		success, pid = self.terminal.fork_command_full(
 			Vte.PtyFlags.NO_WTMP |
 				Vte.PtyFlags.NO_UTMP |
 				Vte.PtyFlags.NO_LASTLOG,
-			SOURCE_TOP,
-			utils.buildCmdArgs(script),
+			self.sourceTop,
+			utils.build_cmd_args(script),
 			(),
 			GLib.SpawnFlags.SEARCH_PATH, None, None)
 		self.pid = pid
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import subprocess
 
 SHELL="/bin/bash"
 
-def checkAndroidSourceTop(directory):
+def check_source_top(directory):
 	main_mk = os.path.sep.join(('', 'build', 'core', 'main.mk'))
 	if not os.access(directory + main_mk, os.R_OK):
 		return False
@@ -12,25 +12,26 @@ def checkAndroidSourceTop(directory):
 		return False
 	return True
 
-def buildCmdArgs(script):
+def build_cmd_args(script):
 	script = "source build/envsetup.sh > /dev/null;" + script
 	return (SHELL, "-c", script)
 
-def getShellOutput(directory, script):
-	cmd = buildCmdArgs(script)
+def get_shell_output(directory, script):
+	cmd = build_cmd_args(script)
 	return subprocess.Popen(cmd, cwd = directory,
 			stdout=subprocess.PIPE,
 			stderr=subprocess.PIPE,
 			close_fds=True).stdout.read()
 	
-def getProductsList(directory):
-	out = getShellOutput(directory, "echo ${LUNCH_MENU_CHOICES[*]}").split()
+def get_product_list(directory):
+	out = get_shell_output(directory, "echo ${LUNCH_MENU_CHOICES[*]}").split()
 	products = []
 	for item in out:
+		if not '-' in item: continue
 		p, variant = item.split('-')
 		if not p in products: products.append(p)
 	return products
 	
 if __name__ == '__main__':
-	print checkAndroidSourceTop(sys.argv[1])
-	print "out = ", getProductsList(sys.argv[1])
+	print check_source_top(sys.argv[1])
+	print "out = ", get_product_list(sys.argv[1])
